ba046fb3421a0706b7c5832c1acf652727c6ff6f
Matt Braddock <matt.braddock@gmail.com>
2020-12-01 10:48:42 -0500

    more efficient

--- scripts/day01.js
+++ scripts/day01.js
@@ -4,12 +4,14 @@ module.exports = {
     name: 'day01',
     exe: () => {
         const input = require('fs').readFileSync(require('path').join(__dirname, '../inputs/day01.txt'), 'utf-8').split('\n').filter(Boolean).map(x => parseInt(x));
+        input.sort((a, b) => a -b); // More efficient with > breaks
 
         // Part One: find two numbers in a list that sum to 2020 and return their product.
         let a, b;
         out1: for (let i = 0; i < input.length - 1; i++) {
             for (let j = i + 1; j < input.length; j++) {
                 const sum = input[i] + input[j];
+                if (sum > 2020) break;
                 if (sum === 2020) {
                     a = input[i];
                     b = input[j];
@@ -25,6 +27,7 @@ module.exports = {
             for (let j = i + 1; j < input.length - 1; j++) {
                 for (let k = i + 2; k < input.length; k++) {
                     const sum = input[i] + input[j] + input[k];
+                    if (sum > 2020) break;
                     if (sum === 2020) {
                         c = input[i];
                         d = input[j];
