e71da9218312c1dda09b0c1a6e06f9c6bc42040e
Matt Braddock <matt.braddock@gmail.com>
2020-12-01 08:20:49 -0500

    shortened code

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const path = require('path');
-const fs = require('fs');
-const rl = require('readline-sync');
-
-const scriptDir = path.join(__dirname, './scripts');
+const scriptDir = require('path').join(__dirname, './scripts');
 let cmd = new Map();
-let imports = fs.readdirSync(scriptDir).filter(file => file.endsWith('.js'));
+let imports = require('fs').readdirSync(scriptDir).filter(file => file.endsWith('.js'));
 imports.forEach(file => {
-    const com = require(path.join(scriptDir, file));
+    const com = require(require('path').join(scriptDir, file));
     cmd.set(com.name, com);
 });
 
 async function start() {
-    const day = rl.question('select day: ');
+    const day = require('readline-sync').question('select day: ');
     try {
         if (/\D+/g.test(day)) throw 'only numbers';
         if (parseInt(day) < 1 || parseInt(day) > 25) throw 'not a valid day';
