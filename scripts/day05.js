9f63e3ebe53922609a3d0d4b386ab4ca9924efbd
Matt Braddock <matt.braddock@gmail.com>
2020-12-05 01:08:55 -0500

    combining day 5 parts

--- scripts/day05.js
+++ scripts/day05.js
@@ -9,22 +9,9 @@ module.exports = {
         // Part one test answer: 820
         // Part two test answer: N/A
 
-        // Part one: determine the highest seat ID (row * 8 + column)
+        // Part one: determine the highest seat ID (row * 8 + column).
+        // Part two: determine your seat, knowing seat IDs adjacent to yours are taken.
         let a = 0;
-        input.forEach(x => {
-            const row = [0, 127];
-            const col = [0, 7];
-            [...x].forEach(y => {
-                if (y === 'F') row[1] -= (row[1] - row[0] + 1) / 2;
-                if (y === 'B') row[0] += (row[1] - row[0] + 1) / 2;
-                if (y === 'L') col[1] -= (col[1] - col[0] + 1) / 2;
-                if (y === 'R') col[0] += (col[1] - col[0] + 1) / 2;
-            });
-            a = Math.max(a, row[0] * 8 + col[0]);
-        });
-        console.log('Part One: ' + a);
-
-        // Part two: 
         const b = [...Array(1023).keys()];
         input.forEach(x => {
             const row = [0, 127];
@@ -35,9 +22,11 @@ module.exports = {
                 if (y === 'L') col[1] -= (col[1] - col[0] + 1) / 2;
                 if (y === 'R') col[0] += (col[1] - col[0] + 1) / 2;
             });
+            a = Math.max(a, row[0] * 8 + col[0]);
             b.splice(b.indexOf(row[0] * 8 + col[0]), 1);
         });
-        const c = b.filter(x => b.indexOf(x - 1) === -1 && b.indexOf(x + 1) === -1);
-        console.log('Part Two: ' + c[0]);
+        const c = b.find(x => b.indexOf(x - 1) === -1 && b.indexOf(x + 1) === -1);
+        console.log('Part One: ' + a);
+        console.log('Part Two: ' + c);
     }
 }
