b1b3243a24be5c32e8442b32b60a137b567c7482
Matt Braddock <matt.braddock@gmail.com>
2020-12-05 01:11:53 -0500

    combining day 2 parts

--- scripts/day02.js
+++ scripts/day02.js
@@ -10,6 +10,7 @@ module.exports = {
         // Part two test answer: 1
 
         // Part one: how many passwords are valid where the number of occurrences of the letter are in the given range.
+        // Part two: how many passwords are valid where the given letter appears in exactly one of the two positions given.
         const a = input.map(x => ({
             'min': parseInt(x.match(/^\d+(?=\-)/)[0]),
             'max': parseInt(x.match(/(?<=\-)\d+/)[0]),
@@ -22,16 +23,8 @@ module.exports = {
             b += x.pwd.match(r) === null ? false : x.pwd.match(r).length >= x.min && x.pwd.match(r).length <= x.max;
         });
         console.log('Part One: ' + b);
-
-        // Part two: how many passwords are valid where the given letter appears in exactly one of the two positions given.
-        const c = input.map(x => ({
-            'pos1': parseInt(x.match(/^\d+(?=\-)/)[0]) - 1,
-            'pos2': parseInt(x.match(/(?<=\-)\d+/)[0]) - 1,
-            'ltr': x.match(/[a-zA-Z](?=\:)/)[0],
-            'pwd': x.match(/[a-zA-Z]+$/)[0]
-        }));
-        let d = 0;
-        c.forEach(x => d += (x.pwd[x.pos1] === x.ltr) !== (x.pwd[x.pos2] === x.ltr));
-        console.log('Part Two: ' + d);
+        let c = 0;
+        a.forEach(x => c += (x.pwd[x.min - 1] === x.ltr) !== (x.pwd[x.max - 1] === x.ltr));
+        console.log('Part Two: ' + c);
     }
 }
